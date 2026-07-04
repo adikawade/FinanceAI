c460fa87de084a8b072f46ea136714b4be2629c8
adikawade <playverse99@gmail.com>
2026-07-04 23:36:17 +0530

    Phase 1: add Application class

---
+++ app/src/main/java/com/financeai/app/FinanceAIApp.kt
@@ -0,0 +1,10 @@
+package com.financeai.app
+
+import android.app.Application
+
+class FinanceAIApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // TODO: Initialize Firebase, dependency injection, logging and analytics.
+    }
+}
