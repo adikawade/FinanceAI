17e69da97c84df49e69a62e69b2fdb1ed7659880
adikawade <playverse99@gmail.com>
2026-07-04 23:39:12 +0530

    Phase 1: add Compose typography definitions

---
+++ app/src/main/java/com/financeai/app/ui/theme/Type.kt
@@ -0,0 +1,9 @@
+package com.financeai.app.ui.theme
+
+import androidx.compose.material3.Typography
+
+/**
+ * Central typography definition for FinanceAI.
+ * This will be expanded into a complete design system.
+ */
+val FinanceTypography = Typography()
