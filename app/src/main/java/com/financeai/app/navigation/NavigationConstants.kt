0dc7b0d60683c1a19d3cfe2db5588481149c6f8d
adikawade <playverse99@gmail.com>
2026-07-04 23:53:51 +0530

    Phase 1: add navigation constants

---
+++ app/src/main/java/com/financeai/app/navigation/NavigationConstants.kt
@@ -0,0 +1,8 @@
+package com.financeai.app.navigation
+
+object NavigationConstants {
+    const val ROOT_GRAPH = "root_graph"
+    const val AUTH_GRAPH = "auth_graph"
+    const val MAIN_GRAPH = "main_graph"
+    const val ANIMATION_DURATION_MS = 300
+}
