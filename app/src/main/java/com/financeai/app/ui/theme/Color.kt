4140a06756e56e96fcd35015c8f1a3c6bef2311d
adikawade <playverse99@gmail.com>
2026-07-04 23:38:57 +0530

    Phase 1: add Compose color definitions

---
+++ app/src/main/java/com/financeai/app/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.financeai.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val FinancePrimary = Color(0xFF2563EB)
+val FinanceSecondary = Color(0xFF10B981)
+val FinanceBackground = Color(0xFFFFFFFF)
+val FinanceSurface = Color(0xFFF8FAFC)
+val FinanceError = Color(0xFFDC2626)
