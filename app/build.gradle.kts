dcbde53f1bb683677146820cbc6f61b51b4babf8
adikawade <playverse99@gmail.com>
2026-07-05 21:42:23 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,9 +13,57 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
 dependencies {
+
     implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.androidx.activity.compose)
+
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation(libs.androidx.compose.ui)
+
+    implementation(libs.androidx.compose.ui.tooling.preview)
+
+    implementation(libs.androidx.compose.material3)
+
+    implementation(libs.androidx.navigation.compose)
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
