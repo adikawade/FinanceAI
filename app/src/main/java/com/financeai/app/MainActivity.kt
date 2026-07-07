bc99c889c26344cb1d73f3075ac5d0bc743e02b5
adikawade <playverse99@gmail.com>
2026-07-07 14:12:20 +0530

    Update MainActivity.kt

--- app/src/main/java/com/financeai/app/MainActivity.kt
+++ app/src/main/java/com/financeai/app/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.financeai.app.navigation.RootNavGraph
 import com.financeai.app.ui.theme.FinanceAITheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             FinanceAITheme {
                 RootNavGraph()
